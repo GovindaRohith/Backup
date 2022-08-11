a6d5f42b1527b5db64752b6eae9622e9a6996892
GovindaRohith <cs21btech11062@iith.ac.in>
2022-08-11 12:20:57 +0530

    Finish_bst

--- bst.c
+++ bst.c
@@ -155,16 +155,21 @@ int main()
     int n=0;
     Root r;
     r.start=NULL;
-    r=insert(r,5);
-    r=insert(r,4);
-    r=insert(r,3);
-    r=insert(r,2);
-    r=insert(r,1);
     r=insert(r,6);
+    r=insert(r,5);
+    r=insert(r,2);
+    r=insert(r,3);
+    r=insert(r,10);
+    r=insert(r,1);
+    r=insert(r,9);
+    r=insert(r,11);
     r=insert(r,7);
     r=insert(r,8);
-    r=insert(r,9);
+    r=insert(r,12);
     printer(r.start);
     printf("\n");
+    delete(r,6);
+    printer(r.start);
+    printf("\n");   
     return 0;
 }
