fd655186406f375dca2f313fddccfa742b71c939
GovindaRohith <cs21btech11062@iith.ac.in>
2022-08-18 10:41:36 +0530

    ass

--- travel.c
+++ travel.c
@@ -1,7 +1,61 @@
 #include<stdio.h>
 #include<stdlib.h>
+struct  node
+{
+    int item;
+    struct node *left,*right; 
+};
+typedef struct node * Node;
+struct root
+{
+    struct node *start;
+};
+typedef struct root Root;
+Root insert(Root e,int n)
+{
+    Node temp=e.start;
+    Node new=malloc(sizeof(Node));
+    new->item=n;
+    new->left=NULL;
+    new->right=NULL;
+    if(temp==NULL)
+    {
+        e.start=new;
+        return e;
+    }
+    else
+    {
+        while(temp!=NULL)
+        {
+            if(n>temp->item)
+            {
+                if(temp->right==NULL)
+                {
+                    temp->right=new; 
+                    return e; 
+                }
+                else temp=temp->right;
+            }
+            else
+            {
+                if(temp->left==NULL)
+                {
+                    temp->left=new;
+                    return e; 
+                }
+                else temp=temp->left;
+            }
+        }
+        return e;
+    }
+}
 int main()
 {
-    
+    printf("Enter any elements to insert");
+    int n;
+    while(scanf("%d",&n)!=-1)
+    {
+        
+    }
     return 0;
 }
