25f70eba469fcdfbada584f6ca37204175dba650
GovindaRohith <cs21btech11062@iith.ac.in>
2022-08-22 22:55:56 +0530

    BHT

---
+++ bht.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+struct node
+{
+    int item;
+    struct node *left,*right;
+};
+typedef struct node *Node;
+struct root
+{
+    Node start;
+};
+typedef struct root Root;
+int min(Root e)
+{
+    return 0;
+}
+Root delete(Root e,int n)
+{
+    return e;
+}
+Root insert(Root e,int n)
+{
+    return e;
+}
+int main()
+{
+    
+    return 0;
+}
