d732e3bb319a73d0d2bd937e4073febd9557a674
GovindaRohith <cs21btech11062@iith.ac.in>
2022-08-30 11:11:00 +0530

    HS

---
+++ sort.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<stdlib.h>
+int * heapsort(int * arr)
+{
+    return arr;
+}
+int main()
+{
+    int n,*arr;
+    arr=(int *)malloc(sizeof(int)*(n+1));
+    //size arr[0]
+    free(arr);
+    return 0;
+}
