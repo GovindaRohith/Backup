5bf34ae1f1eb56b89d19f479550e1e708a526dbe
GovindaRohith <cs21btech11062@iith.ac.in>
2022-08-31 12:07:05 +0530

    BST

--- implement/a5.cpp
+++ implement/a5.cpp
@@ -1,7 +1,7 @@
 // 1.  Each node contains the Age, Name, Gender, and
 // Department of a faculty.
 // Assume the Name is unique
-
+// In-order traversal of threaded binary trees
 // 2.  You should implement
 // A.Insert
 // B.Delete by Name
--- implement/a6.cpp
+++ implement/a6.cpp
@@ -1,5 +1,5 @@
 // 1.Each node contains the Age of a faculty.
-
+//Implementation of Heap
 // 2.You should implement
 // A.Insert
 // B.Find Min
