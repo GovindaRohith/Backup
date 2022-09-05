1015803e4e4f0998fc3a0fb2fc242d527667ff23
GovindaRohith <cs21btech11062@iith.ac.in>
2022-09-05 21:55:11 +0530

    Comp

--- implement/a6.cpp
+++ implement/a6.cpp
@@ -4,3 +4,99 @@
 // A.Insert
 // B.Find Min
 // C.Delete Min
+#include<iostream>
+#include<stdlib.h>
+#define INIT 100
+using namespace std;
+int min(int *arr)
+{
+    if(arr!=NULL) return arr[1];
+    else return -1;
+}
+
+void printer(int *arr)
+{
+    int i;
+    for(i=1;i<=arr[0];i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+//      parent
+//         i
+//   c1           c2
+//   2*i          2*i+1 
+// parent < children
+int * insert(int *arr,int n)
+{
+    // index current size given
+    int index=arr[0],temp;
+    if(index+1==99) 
+    {
+        printf("Array full");
+        return arr;
+    }
+    arr[index+1]=n;
+    index++;
+    while(index>1)
+    {
+        if(n<arr[index/2]&&index/2>=1)
+        {
+            temp=arr[index/2];
+            arr[index/2]=arr[index];
+            arr[index]=temp;
+            index=index/2;
+        }
+        else 
+        {
+            break;
+        }
+    }
+    arr[0]++;
+    return arr;
+}
+int * delete_c(int *arr)
+{
+    int index=1,temp;
+    arr[1]=arr[arr[0]];
+    while(index<=arr[0]&&(2*index+1)<=arr[0])//Equailty verify
+    {   
+        if(arr[index*2+1]>arr[index*2])
+        {
+            temp=arr[index];
+            arr[index]=arr[index*2];
+            arr[index*2]=temp;
+            index=index*2;
+        }
+        else if(arr[index*2]>arr[2*index+1])
+        {
+            temp=arr[index];
+            arr[index]=arr[index*2+1];
+            arr[index*2+1]=temp;
+            index=index*2+1;
+        }
+        else
+        {       
+            break;
+        }   
+    }
+    arr[0]=arr[0]-1;
+    return arr;
+}
+int main()
+{
+    // arr[0] ==>INIT
+    int *arr;
+    arr=(int *)malloc(sizeof(int)*INIT);
+    arr[0]=0;
+    arr=insert(arr,8);
+    arr=insert(arr,23);
+    arr=insert(arr,48);
+    printer(arr);
+    printf("The minimum value of the BHT is %d \n",min(arr));
+    arr=delete_c(arr);
+    printer(arr);
+    free(arr);
+    return 0;
+}
